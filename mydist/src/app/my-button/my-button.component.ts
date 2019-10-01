779965e92f9939fe76924829688c047264480661
Dejiang Chen <dejiang27@gmail.com>
2019-10-01 18:54:55 -0500

    Cleaned my-button

--- mydist/src/app/my-button/my-button.component.ts
+++ mydist/src/app/my-button/my-button.component.ts
@@ -9,20 +9,16 @@ import { MyServiceService } from '../my-service.service';
 export class MyButtonComponent implements OnInit {
 
   likeImage:boolean;
-  classesObj:boolean;
   constructor(private myservice:MyServiceService) { }
 
   ngOnInit() {
     this.likeImage = false;
-    this.classesObj = this.likeImage;
   }
-
 
   clicked(){
     this.likeImage = this.myservice.likeImage(this.likeImage);
     //Test the like button. Show the result in the console.
     console.log(this.likeImage);
-    this.classesObj = this.likeImage;
     return this.likeImage;
   }
 
